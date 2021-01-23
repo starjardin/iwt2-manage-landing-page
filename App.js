bfa981998fce7ac1d14c9e09773064465d649d8a
Tantely Andrianarivola <tantely.and@onja.org>
2021-01-23 09:19:50 +0300

    getting started

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <h2>
+      I am the app component
+    </h2>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { defalut as Header } from './components/header'
